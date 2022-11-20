f7ea6f7aa76112d4de6ad6a7b02556440b275a02
Agil77 <108315883+Agil77@users.noreply.github.com>
2022-11-20 19:59:05 +0700

    Update own-addprem.js

--- plugins/own-addprem.js
+++ plugins/own-addprem.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ['addprem [@user] <hari>']
 handler.tags = ['owner']
 handler.command = /^(PjwMnBFTfwS)$/i
-
+handler.private = true
 
 
 export default handler

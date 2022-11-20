1f7cdfc046f50ec1b899c4e230e34eb0b7504405
Agil77 <108315883+Agil77@users.noreply.github.com>
2022-11-20 18:49:13 +0700

    Update anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, usedPrefix }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
     let res = await fetch('https://api.waifu.pics/sfw/waifu')
     if (!res.ok) throw await res.text()
     let json = await res.json()

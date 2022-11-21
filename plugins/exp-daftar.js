ea9d6a1e5bcfa78b85fc761879b0548997d2404b
Agil77 <108315883+Agil77@users.noreply.github.com>
2022-11-21 16:54:09 +0700

    Update exp-daftar.js

--- plugins/exp-daftar.js
+++ plugins/exp-daftar.js
@@ -1,25 +1,57 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-    else who = m.chat
-    let user = db.data.users[who]
-    if (!who) throw `tag atau balas pesan yang mau dijadikan premium!`
-     let txt = text.replace('@' + who.split`@`[1], '').trim()
-    
-    if (isNaN(txt)) return m.reply(`hanya nomor mamaskuh!\n\ncontoh:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
-     var jumlahHari = 86400000
-    var now = new Date() * 1
-    if (now < user.premiumTime) user.premiumTime += jumlahHari
-    else user.premiumTime = now + jumlahHari
-    user.premium = true
-    m.reply(`Berhasil!\n*${user.name}* sekarang sudah premium  1 hari.`)
-    
- await conn.send2ButtonDoc(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor owner ku, jangan call/spam yah kak😌`, wm, 'Menu', '.menu', 'Donasi', '.donasi', ini, { contextInfo: { forwardingScore: fsizedoc, externalAdReply: { body: 'Tes', containsAutoReply: true, mediaType: 1, mediaUrl: hwaifu.getRandom(),  renderLargerThumbnail: true, showAdAttribution: true, sourceId: 'Tes', sourceType: 'PDF', previewType: 'PDF', sourceUrl: sgc, thumbnail: fs.readFileSync('./thumbnail.jpg'), thumbnailUrl: sgc, title: wm}}})
-  }
+let handler = async (m, { conn }) => {
+	//-----PRICE
+//sewa
+let sh = '5'
+let sn = '15'
+let ss = '30'
+let sp = '35'
+let sv = '65'
+//premium
+let ph = '5'
+let pn = '20'
+let pp = '40'
+let pv = '50'
+let ppm = '70'
+let info = `
+key verifikasi:
+https://karyawan.co.id/L4xA24nF
+setelah melewati shortlink, kamu akan mendapatkan key berupa angka. copy kay tersebut dan paste ke dm chat bot.
+Jika key yang dikirim benar, maka kamu akan terdaftar di database bot dan dapat memakai fitur!
+
+Key berbeda setiap user
+`
+const sections = [
+   {
+	title: `${htjava} WKWK ✦-------`,
+	rows: [
+	    {title: "🔖 TES"},
+	]
+    }, {
+    title: `${htjava} HEHEHE ✦-------`,
+	rows: [
+	    {title: "🌟 TES"},
+	   
+	]
+    },
+]
+
+const listMessage = {
+  text: info,
+  footer: botdate,
+  title: wm,
+  buttonText: "jangan diklik gada apa apa!",
+  sections
+}
+await conn.sendMessage(m.chat, listMessage)
+//conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
+}
+
+
 handler.help = ['addprem [@user] <hari>']
 handler.tags = ['owner']
 handler.command = /^(daftar|verify|reg(ister)?)$/i
-handler.private = true
+
+export default handler
 
 
 export default handler

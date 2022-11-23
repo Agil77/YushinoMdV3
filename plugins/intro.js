f3076f9dfca9b2b019fae3ed2ff154d17126a716
Agil77 <108315883+Agil77@users.noreply.github.com>
2022-11-23 15:06:47 +0700

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -11,18 +11,7 @@ let krtu = `☰⃟⟣⟔ Yᴏᴜʀ Cᴀʀᴅ Iɴᴛʀᴏ ⟓⟢⃟
 ┇⃟⟣⟜ *Pasangan:*
 ┖┈┈┈╼╸╸╸╴╴╴ ╳
 `
-let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
-let thumb = await(await fetch(wibu)).buffer()
-conn.sendButtonDoc(m.chat, krtu, wm,'MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://facebook.com/sadtime098",
-    mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'Iɴᴛʀᴏᴄᴀᴅᴜᴛɪᴏɴ',
-    body: wm,
-    thumbnail: thumb,
-    sourceUrl: sgc
-  }
-  } }) // Tambah sendiri kalo mau
+ // Tambah sendiri kalo mau
 }
 handler.command = /^(intro)$/i
 

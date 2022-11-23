ce2a774ac4bb53a3347a3b95625a08d314913d4b
Agil77 <108315883+Agil77@users.noreply.github.com>
2022-11-23 14:16:53 +0700

    Update sticker-meme.js

--- plugins/sticker-meme.js
+++ plugins/sticker-meme.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ['smeme <teks atas>|<teks bawah>']
 handler.tags = ['tools']
 handler.command = /^(smeme)$/i
-handler.premium = true
+handler.premium = false
 
 handler.limit = true
 

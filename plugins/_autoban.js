a7b9e0c1220c8d19947199b1dbe9003059044845
Agil77 <108315883+Agil77@users.noreply.github.com>
2022-11-26 14:48:42 +0700

    Update _autoban.js

--- plugins/_autoban.js
+++ plugins/_autoban.js
@@ -8,9 +8,6 @@ handler.before = async function (m) {
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
    }
-   if (m.sender.finishWith('9625' || '9625')) {
-   	global.db.data.users[m.sender].banned = true
-   }
     }
 
 export default handler
